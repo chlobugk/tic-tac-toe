179d4908426772f41350e2d94974175ceb5d0f1b
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-03-06 15:45:37 -0500

    automatically take win function

---
+++ test_unbeatable.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require_relative 'unbeatable.rb'
+
+class TestUnbeatable < MiniTest::Test 
+
+	def test_new_player_x
+		player = Unbeatable_AI.new('x')
+		assert_equal('x', player.marker)
+	end
+
+	def test_win
+		player = Unbeatable_AI.new('x') 
+		ttt_board = ['x', 'x', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+		assert_equal(2, player.fill_move(ttt_board))
+	end
+
+	def test_win_at_last_pos
+		player = Unbeatable_AI.new('x') 
+		ttt_board = [' ', ' ', ' ', ' ', ' ', ' ', 'x', 'x', ' ']
+		assert_equal(8, player.fill_move(ttt_board))
+	end
+
+	def test_win_at_4
+		player = Unbeatable_AI.new('x') 
+		ttt_board = ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'x']
+		assert_equal(4, player.fill_move(ttt_board))
+	end
+
+	def test_no_win_x
+		player = Unbeatable_AI.new('x') 
+		ttt_board = ['x', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+		assert_equal(1, player.fill_move(ttt_board))
+	end
+
+	def test_win_at_5
+		player = Unbeatable_AI.new('o') 
+		ttt_board = [' ', ' ', ' ', 'o', 'o', ' ', ' ', ' ', ' ']
+		assert_equal(5, player.fill_move(ttt_board))
+	end
+
+	def test_no_win_o
+		player = Unbeatable_AI.new('o') 
+		ttt_board = [' ', 'o', ' ', ' ', ' ', ' ', ' ', ' ', 'o']
+		assert_equal(0, player.fill_move(ttt_board))
+	end
+
+end
---
+++ unbeatable.rb
@@ -0,0 +1,44 @@
+class Unbeatable_AI
+
+	attr_reader :marker
+
+	def initialize(marker)
+		@marker = marker
+	end
+
+	def fill_move(ttt_board)
+		win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]] 
+		
+			board_pos = [
+				[ttt_board[0], ttt_board[1], ttt_board[2]],
+				[ttt_board[3], ttt_board[4], ttt_board[5]],
+				[ttt_board[6], ttt_board[7], ttt_board[8]],
+				[ttt_board[0], ttt_board[3], ttt_board[6]],
+				[ttt_board[1], ttt_board[4], ttt_board[7]],
+				[ttt_board[2], ttt_board[5], ttt_board[8]],
+				[ttt_board[0], ttt_board[4], ttt_board[8]],
+				[ttt_board[2], ttt_board[4], ttt_board[6]]
+						]
+			results = ttt_board.index(' ')
+			board_pos.each_with_index do |win_combo, index|
+				p win_combo
+				if win_combo.count(marker) == 2 && win_combo.count(' ') == 1
+					winning_index = win_combo.index(' ')
+					results = win_array[index][winning_index]
+				else
+					results
+				end
+			end
+			results
+
+	
+
+	end
+
+		
+		
+
+
+
+
+end
