c636e5271cd8f64c251bd3cadf0d95357ace532b
Chloe Krainbucher <chloekrainbucher@Chloes-MacBook-Pro.local>
2017-03-14 12:54:28 -0400

    asks for players in app

--- ttt_app.rb
+++ ttt_app.rb
@@ -2,14 +2,38 @@ require 'sinatra'
 require_relative 'play_game'
 
 get '/' do 
-	erb :p1_input
+	erb :players_input
 end
 
-post '/player1' do
+post '/players' do
 
-	player1 = params[:p1_input].to_i
-	player2 = params[:p2_input].to_i
-	p1 = choose_p1
-	p2 = choose_p2
+	player1 = params[:p1_input]
+	player2 = params[:p2_input]
+	erb :board
 
 end
+
+post '/board' do
+	backend_p1 = params[:p1_input].to_i
+	backend_p2 = params[:p2_input].to_i
+
+		if backend_p1 == 1
+			p1 = User.new('x')
+		elsif backend_p1 == 2
+			p1 = Sequential_AI.new('x')
+		elsif backend_p1 == 3
+			p1 = Random_AI.new('x')
+		elsif backend_p1 == 4
+			p1 = Unbeatable_AI.new('x')
+		else
+			puts "Oops, please pick 1, 2, 3, or 4."
+			choose_p1		
+		end
+end
+
+
+
+
+
+
+
