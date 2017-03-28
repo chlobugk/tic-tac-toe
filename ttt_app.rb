5f881792e7bedbc66fbadebce4d8a6628bcf2a3a
Chloe Krainbucher <chloekrainbucher@pOiuytr-easd-bh78oohb8ubh8.local>
2017-03-28 16:31:35 -0400

    says player who wins

--- ttt_app.rb
+++ ttt_app.rb
@@ -75,7 +75,7 @@ end
 get '/check_game' do
 
 	if session[:board].winner?(session[:active_player].marker)
-		erb :win
+		erb :win, :locals => {:active_player => session[:active_player].marker}
 	elsif session[:board].full_board?
 		erb :tie
 	else
@@ -100,25 +100,6 @@ get '/change_player' do
 	end
 end
 
-
-
-
-
-
-
-
-# redirect '/players?opponent=' + opponent + ''
-	# backend_p1 = params[:p1]
-	# backend_p2 = params[:p2]
-	# position0 = params[:pos0]
-	# position1 = params[:pos1]
-	# position2 = params[:pos2]
-	# position3 = params[:pos3]
-	# position4 = params[:pos4]
-	# position5 = params[:pos5]
-	# position6 = params[:pos6]
-	# position7 = params[:pos7]
-	# position8 = params[:pos8]
 
 
 
