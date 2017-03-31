cdab18a34ec614bc542e2b2e3119f229a859bf7d
Chloe Krainbucher <chloekrainbucher@pOiuytr-easd-bh78oohb8ubh8.local>
2017-03-31 16:07:06 -0400

    added tests unbeatable app

--- test_unbeatable_app.rb
+++ test_unbeatable_app.rb
@@ -33,5 +33,15 @@ class TestUnbeatableInput < MiniTest::Test
 		assert_equal(8, player.opposite_corner(['O',2,3,4,5,6,7,8,9]))
 	end
 
+	def test_empty_corner
+		player = Unbeatable_App.new('O')
+		assert_equal(8, player.empty_corner(['X',2,'O',4,5,6,'X',8,9]))
+	end
+
+	def test_empty_side
+		player = Unbeatable_App.new('O')
+		assert_equal(1, player.empty_side(['X',2,'O','X',5,6,'O',8,'X']))
+	end
+
 
 end
